1e3c172d5e5a2a2ead6a5adfc1356975c75f5c60
Alef <alef@dinossour.com.br>
2019-04-12 11:52:22 -0300

    Minor updates on ui

--- GameJam/Program.cs
+++ GameJam/Program.cs
@@ -71,8 +71,14 @@ namespace GameJam
 
         private static void GetData()
         {
-            Data = JsonConvert.DeserializeObject<GameData>(File.ReadAllText("data.json"));
-
+            try
+            {
+                Data = JsonConvert.DeserializeObject<GameData>(File.ReadAllText("data.json"));
+            }
+            catch
+            {
+                NewGame();
+            }
         }
 
         //Runs after Esc pressed, defines the closing behaviour.
@@ -96,13 +102,12 @@ namespace GameJam
         // On this call, defines all the default values for the properties
         private static void SetUpVars()
         {
-
+            Console.SetWindowSize(136, 27);
             Red = Color.FromArgb(255, 82, 82);
             White = Color.FromArgb(255, 255, 255);
             Green = Color.FromArgb(81, 255, 178);
             Colors = new Color[20];
             Names = new string[20];
-            //GenerateEmptyData();
             Exit = false;
             Breads = Data.Breads;
             BreadsPerSecond = Data.BreadsPerSecond;
@@ -111,34 +116,6 @@ namespace GameJam
 
 
         }
-
-        //private static void GenerateEmptyData()W
-        //{
-        //    var upgrades = new List<Upgrade>();
-        //    for (int i = 0; i < 20; i++)
-        //    {
-        //        upgrades.Add(new Upgrade
-        //        {
-        //            Bought = false,
-        //            Cost = 0,
-        //            CostText = "0",
-        //            Name = "unknow",
-        //            PPSUpgrade = 0,
-        //            Id = i
-        //        });
-        //    }
-        //    var gameData = new GameData()
-        //    {
-        //        Breads = 0,
-        //        BreadsPerClick = 1,
-        //        BreadsPerSecond = 0,
-        //        Upgrades = upgrades
-        //    };
-
-        //    var json = JsonConvert.SerializeObject(gameData);
-        //    File.WriteAllText("data.json", json);
-        //}
-
 
         //This function runs every x mileseconds, defined on the SetUpVars call.
         private static void Update()
@@ -220,54 +197,53 @@ namespace GameJam
         static void Construct()
         {
 
-            string paesString = Breads + " Pães";
-            string sec = BreadsPerSecond + " Pães por Segundo";
-            string click = BreadsPerClick   + " Pães por Clique";
-            Console.WriteLine("                                      Patricio's Bakery ");
-            Console.WriteLine("     ~~~Statistics~~~                   ___________");
-            Console.WriteLine("{0,-39}(   )_______)", paesString);
-            Console.WriteLine("{0,-39}|   |Clique!|", sec);
-            Console.WriteLine("{0,-39}|___|_______|\n", click);
-            Console.WriteLine("                                             ~~~Upgrades~~~");
-            Console.WriteLine("                                                          	    @*#* ");
-            Console.WriteLine("                                     +                            _*                     _____ ");
-            Console.WriteLine("          (o_                        A_                      _   | |                    /    /|");
-            Console.WriteLine("          //\\                       /\\-\\                    | |__| |                   /    //");
-            Console.WriteLine("          V_/_                     _||_|_                 __|      |__                (====|/ \n");
-            Console.WriteLine("          (A)                        (S)                       (D)                       (F) \n", Color.FromArgb(81, 255, 178));
-            Console.WriteLine("--------AJUDANTES--------|---------IGREJA----------|---------FÁBRICA---------|--------PESQUISA---------");
+           
+            Console.WriteLine("                                                            Patricio's Bakery ");
+            Console.WriteLine("     ~~~Statistics~~~ ");
+            Console.Write("{0,-62}  ___________\n","");
+            Console.Write("{0,7} ",Breads,Green); Console.Write("{0,-54} (   )_______)\n","Pães");
+            Console.Write("{0,7} ", BreadsPerSecond,Green); Console.Write("{0,-54} |   |Clique!|\n","Pães Por Segundo");
+            Console.Write("{0,7} ", BreadsPerClick, Green); Console.Write("{0,-54} |___|_______|\n","Pães Por Click");
+            Console.WriteLine("\n\n\n                                                            ~~~Upgrades~~~\n");
+            Console.WriteLine("                                                          	                        @*#* ");
+            Console.WriteLine("                                                 +                                    _*                             _____ ");
+            Console.WriteLine("              (o_                                A_                              _   | |                            /    /|");
+            Console.WriteLine("              //\\                               /\\-\\                            | |__| |                           /    //");
+            Console.WriteLine("              V_/_                             _||_|_                         __|      |__                        (====|/ \n");
+            Console.WriteLine("              (A)                                (S)                               (D)                               (F) \n", Color.FromArgb(81, 255, 178));
+            Console.WriteLine("------------AJUDANTES------------|-------------IGREJA--------------|-------------FÁBRICA-------------|------------PESQUISA-------------");
 
             // Line 1
-            Console.Write("{0,-24}{1}|", Names[0], Data.Upgrades[0].CostText, Colors[0]);
-            Console.Write("{0,-24}{1}|", Names[1], Data.Upgrades[1].CostText, Colors[1]);
-            Console.Write("{0,-24}{1}|", Names[2], Data.Upgrades[2].CostText, Colors[2]);
-            Console.Write("{0,-24}{1}\n", Names[3], Data.Upgrades[3].CostText, Colors[3]);
+            Console.Write("{0,-28}{1}", Names[0], Data.Upgrades[0].CostText, Colors[0]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[1], Data.Upgrades[1].CostText, Colors[1]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[2], Data.Upgrades[2].CostText, Colors[2]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[3], Data.Upgrades[3].CostText, Colors[3]); Console.Write("\n", White);
 
             // Line 2
-            Console.Write("{0,-23}{1}|", Names[4], Data.Upgrades[4].CostText, Colors[4]);
-            Console.Write("{0,-23}{1}|", Names[5], Data.Upgrades[5].CostText, Colors[5]);
-            Console.Write("{0,-23}{1}|", Names[6], Data.Upgrades[6].CostText, Colors[6]);
-            Console.Write("{0,-23}{1}\n", Names[7], Data.Upgrades[7].CostText, Colors[7]);
+            Console.Write("{0,-28}{1}", Names[4], Data.Upgrades[4].CostText, Colors[4]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[5], Data.Upgrades[5].CostText, Colors[5]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[6], Data.Upgrades[6].CostText, Colors[6]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[7], Data.Upgrades[7].CostText, Colors[7]); Console.Write("\n", White);
 
             // Line 3
-            Console.Write("{0,-22}{1}|", Names[8], Data.Upgrades[8].CostText, Colors[8]);
-            Console.Write("{0,-22}{1}|", Names[9], Data.Upgrades[9].CostText, Colors[9]);
-            Console.Write("{0,-22}{1}|", Names[10], Data.Upgrades[10].CostText, Colors[10]);
-            Console.Write("{0,-22}{1}\n", Names[11], Data.Upgrades[11].CostText, Colors[11]);
+            Console.Write("{0,-28}{1}", Names[8], Data.Upgrades[8].CostText, Colors[8]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[9], Data.Upgrades[9].CostText, Colors[9]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[10], Data.Upgrades[10].CostText, Colors[10]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[11], Data.Upgrades[11].CostText, Colors[11]); Console.Write("\n", White);
             // Line 4
-            Console.Write("{0,-22}{1}|", Names[12], Data.Upgrades[12].CostText, Colors[12]);
-            Console.Write("{0,-22}{1}|", Names[13], Data.Upgrades[13].CostText, Colors[13]);
-            Console.Write("{0,-22}{1}|", Names[14], Data.Upgrades[14].CostText, Colors[14]);
-            Console.Write("{0,-22}{1}\n", Names[15], Data.Upgrades[15].CostText, Colors[15]);
+            Console.Write("{0,-28}{1}", Names[12], Data.Upgrades[12].CostText, Colors[12]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[13], Data.Upgrades[13].CostText, Colors[13]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[14], Data.Upgrades[14].CostText, Colors[14]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[15], Data.Upgrades[15].CostText, Colors[15]); Console.Write("\n", White);
             // Line 5
-            Console.Write("{0,-21}{1}|", Names[16], Data.Upgrades[16].CostText, Colors[16]);
-            Console.Write("{0,-21}{1}|", Names[17], Data.Upgrades[17].CostText, Colors[17]);
-            Console.Write("{0,-21}{1}|", Names[18], Data.Upgrades[18].CostText, Colors[18]);
-            Console.Write("{0,-21}{1}\n", Names[19], Data.Upgrades[19].CostText, Colors[19]);
+            Console.Write("{0,-28}{1}", Names[16], Data.Upgrades[16].CostText, Colors[16]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[17], Data.Upgrades[17].CostText, Colors[17]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[18], Data.Upgrades[18].CostText, Colors[18]); Console.Write("|", White);
+            Console.Write("{0,-28}{1}", Names[19], Data.Upgrades[19].CostText, Colors[19]); Console.Write("\n", White);
+            
 
 
-
-            Console.Write("\n                                           DINOSSOUR ", Color.FromArgb(81, 255, 178));
+            Console.Write("\n                                                           DINOSSOUR ", Color.FromArgb(81, 255, 178));
             Console.Write("GAMES ", Color.FromArgb(164, 82, 255));
         }
 
@@ -275,9 +251,6 @@ namespace GameJam
         //All the Checkings that needs to run every frame goes here, this is the first method that run on Update().
         private static void CheckStuff()
         {
-
-
-
         }
         #endregion
 
@@ -286,7 +259,7 @@ namespace GameJam
         //This is the cherry on top of the cake, this method handle the mouse clicks and has ONLY ONE FUNCTION, update the Click property... ^_^
         private static void MouseHook_OnMouseEvent(VirtualKeyCode key, KeyState state, int x, int y)
         {
-            if (state == KeyState.Down) Breads+= BreadsPerClick;
+            if (state == KeyState.Down) Breads += BreadsPerClick;
         }
 
         //This method deals with all the keyboard inputs, once the Console.Reads doesn't work anymore. 
@@ -505,26 +478,18 @@ namespace GameJam
                             }
                         }
                         break;
-                        //case VirtualKeyCode.F5:
-                        //    NewGame();
-                        //    break;
+                    case VirtualKeyCode.F5:
+                        NewGame();
+                        break;
                 }
 
             }
         }
-        //private static void NewGame()
-        //{
-        //    var datinha = JsonConvert.DeserializeObject<GameData>(File.ReadAllText("user.dll"));
-        //    var json = JsonConvert.SerializeObject(datinha);
-        //    if (!File.Exists("data.json"))
-        //    {
-        //        FileStream str = File.Create("data.json");
-        //        str.Close();
-        //        File.WriteAllText("data.json", json);
-        //    }
-        //    GetData();
-        //    SetUpVars();
-        //}
+        private static void NewGame()
+        {
+            Data = JsonConvert.DeserializeObject<GameData>(File.ReadAllText("user.dll"));
+            SetUpVars();
+        }
 
         static void Comprar(int position, params bool[] last)
         {
